dc12e73a5ada2234a96b133c7357ed7d3f543ef4
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-29 10:02:08 +0500

    done in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { gql, useQuery } from '@apollo/client'
+
+
+const query = gql`
+  query GetTodosWithUser {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, loading } = useQuery(query);
+  const [todos,setTodos] = useState([])
 
+  useEffect(()=>{
+    setTodos(data.getTodos)
+    console.log(data.getTodos);
+  },[data])
+
+  if(loading){
+    return (
+      <div>Loading...</div>
+    )
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>The todos</h1>
+      <ul>
+        {
+          todos.length > 0 && todos.map(todo=>(
+            <li key={todo.id} ><span>{todo.title}</span>--   (<span>{todo.user.name}</span>) -- {todo.completed ? <span style={{color:'green'}} >Done</span> : <span style={{color:'red'}} >Pending</span>} </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
